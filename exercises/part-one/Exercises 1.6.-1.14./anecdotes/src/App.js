58ae5b54554658710103931af340b93faa62fce2
Tulio Minini <tulio.mminini@gmail.com>
2022-08-25 12:04:20 -0300

    :sparkles: anecdotes  exercise resolution

--- exercises/part-one/Exercises 1.6.-1.14./anecdotes/src/App.js
+++ exercises/part-one/Exercises 1.6.-1.14./anecdotes/src/App.js
@@ -1,88 +1,57 @@
 import { useState } from "react";
 
-const Button = (props) => {
-  // console.log(props);
-  return (
-    <div>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </div>
-  );
-};
-
-const Display = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <div>{props.selected.anecdote}</div>
-      <div>has {props.selected.vote} votes</div>{" "}
-    </div>
-  );
-};
-// const DisplayVotes = (props) => <div>has {props.vote} votes</div>;
+const Anecdote = ({ text, vote_count }) => (
+  <>
+    {text}
+    <div>has {vote_count} votes</div>
+  </>
+);
 
 const App = () => {
   const anecdotes = [
-    {
-      anecdote: "If it hurts, do it more often.",
-      vote: [],
-    },
-    {
-      anecdote: "Adding manpower to a late software project makes it later!",
-      vote: [],
-    },
-    {
-      anecdote:
-        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-      vote: [],
-    },
-    {
-      anecdote:
-        "Any foos can write code that a computer can understand. Good programers write code that humans can understand.",
-      vote: [],
-    },
-    {
-      anecdote: "Premature optimization is the root of all evil.",
-      vote: [],
-    },
-    {
-      anecdote:
-        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-      vote: [],
-    },
-    {
-      anecdote:
-        "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-      vote: [],
-    },
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any foos can write code that a computer can understand. Good programers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
   ];
 
-  const getRandom = () => {
-    return anecdotes[Math.floor(Math.random() * anecdotes.length)];
-  };
-  const [selected, setSelected] = useState(getRandom());
-  const [votes, setVotes] = useState(0);
-  // const [allPoints, setAllPoints] = useState([]);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0]);
 
-  const handleRandom = () => {
-    // console.log(props);
-    return setSelected(getRandom());
+  let most_voted = 0;
+  for (let i = 0; i < anecdotes.length; i++) {
+    if (votes[i] > votes[most_voted]) {
+      most_voted = i;
+    }
+  }
+
+  const next = () => {
+    let newSelected = selected;
+    while (newSelected === selected) {
+      newSelected = Math.floor(Math.random() * anecdotes.length);
+    }
+    setSelected(newSelected);
   };
 
-  const handleVote = (props) => {
-    console.log(props);
-    return setVotes(props.vote.push(...selected.vote, 1));
+  const vote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
   };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <Display selected={selected} />
-      {/* <DisplayVotes vote={vote.vote} /> */}
+      <h3>Anecdote of the day</h3>
+      <Anecdote text={anecdotes[selected]} vote_count={votes[selected]} />
       <div>
-        <Button handleClick={handleVote} text="vote" />
-        <Button handleClick={handleRandom} text="next anecdote" />
+        <button onClick={vote}>vote</button>
+        <button onClick={next}>next anecdote</button>
       </div>
-      <h1>Anecdote with most votes</h1>
+      <h3>Anecdote with most votes</h3>
+      <Anecdote text={anecdotes[most_voted]} vote_count={votes[most_voted]} />
     </div>
   );
 };
