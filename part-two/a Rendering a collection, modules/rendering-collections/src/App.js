4a1dcb30243f68af90d19e66b704c51105532475
Tulio Minini <tulio.mminini@gmail.com>
2022-08-25 23:26:12 -0300

    :books: application created to follow along with part2 a Rendering Collections

--- part-two/a Rendering a collection, modules/rendering-collections/src/App.js
+++ part-two/a Rendering a collection, modules/rendering-collections/src/App.js
@@ -1,16 +1,21 @@
-const App = (props) => {
-  const { notes } = props;
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
+        {/* <li>{notes[0].content}</li>
         <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        <li>{notes[2].content}</li> */}
+        {/* {notes.map((currentNote) => (
+          <li key={currentNote.id}>{currentNote.content}</li>
+        ))} */}
+        {notes.map((currentNote) => (
+          <Note key={currentNote.id} currentNote={currentNote} />
+        ))}
       </ul>
     </div>
   );
 };
-
 export default App;
---
+++ part-two/a Rendering a collection, modules/rendering-collections/src/components/Note.js
@@ -0,0 +1,5 @@
+import { React } from "react";
+
+const Note = ({ currentNote }) => <li>{currentNote.content}</li>;
+
+export default Note;
