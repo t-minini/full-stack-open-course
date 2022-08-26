a3a79d27383adbc08ebeca4eced09b645f6d583c
Tulio Minini <tulio.mminini@gmail.com>
2022-08-26 03:16:49 -0300

    :books: 2.2: Course information step7 AND 2.3*: Course information step8

--- exercises/part-two/Exercises 2.1.-2.5./courseinfo-v2/src/components/Course.js
+++ exercises/part-two/Exercises 2.1.-2.5./courseinfo-v2/src/components/Course.js
@@ -12,8 +12,25 @@ const Course = ({ course }) => {
       <Part part={parts[0]} />
       <Part part={parts[1]} />
       <Part part={parts[2]} />
+      <Part part={parts[3]} />
+      <Total part={total} />
     </>
   );
+
+  const total = course.parts.reduce((s, p) => {
+    return { exercises: s.exercises + p.exercises };
+  });
+  // console.log(total);
+
+  const Total = (props) => {
+    // console.log(props);
+    return (
+      <p>
+        <strong>total of {props.part.exercises} exercises</strong>
+      </p>
+    );
+  };
+
   return (
     <div>
       <Header course={course.name} />
