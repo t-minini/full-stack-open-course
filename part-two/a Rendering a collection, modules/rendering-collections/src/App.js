642020f5a7b17ca76e8340d1c2a8048b2f21236f
Tulio Minini <tulio.mminini@gmail.com>
2022-08-27 17:13:58 -0300

    :books: Notes application updated to follow along with b
    Forms Part2

--- part-two/a Rendering a collection, modules/rendering-collections/src/App.js
+++ part-two/a Rendering a collection, modules/rendering-collections/src/App.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote(" ");
+  };
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
         {/* <li>{notes[0].content}</li>
         <li>{notes[1].content}</li>
@@ -11,10 +39,14 @@ const App = ({ notes }) => {
         {/* {notes.map((currentNote) => (
           <li key={currentNote.id}>{currentNote.content}</li>
         ))} */}
-        {notes.map((currentNote) => (
+        {notesToShow.map((currentNote) => (
           <Note key={currentNote.id} currentNote={currentNote} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
